9556e9ea8d37d1f2cf392727c8d793ff855c6261
ihs8 <ihebstudent@gmail.com>
2022-03-20 02:44:59 +0100

    init

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ahlaa ya Majdi .
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ihs8/fleet_tracking"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          tauri fleet v0
         </a>
       </header>
     </div>
